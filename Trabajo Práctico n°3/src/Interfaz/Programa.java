5fb1160cf1322fe07d0132520ecf1e6926eca3af
Agustin1432 <agustin72encina@gmail.com>
2020-09-20 15:29:32 -0300

    Ajustes de Interfaz 1

---
+++ Trabajo Práctico n°3/src/Interfaz/Programa.java
@@ -0,0 +1,11 @@
+package Interfaz;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		VentanaVentas v1 = new VentanaVentas();
+		v1.setVisible(true);
+
+	}
+
+}
--- Trabajo Práctico n°3/src/Interfaz/VentanaVentas.java
+++ Trabajo Práctico n°3/src/Interfaz/VentanaVentas.java
@@ -1,5 +1,70 @@
 package Interfaz;
 
-public class VentanaVentas {
+import java.awt.Container;
+//import java.awt.Font;
+//import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+
+public class VentanaVentas extends JFrame {
+	/*tipos de interfaces
+	 * JButton			BOTON
+	 * JTextField		CUADRO DE TEXTO
+	 * JFrame			VENTANAS
+	 * JPanel			PANELES
+	 * JLabel			ETIQUETAS
+	 * JRadioButton 	BOTON DE OPCION
+	 * 
+	 */
+	public JPanel p1;
+	public Container cp;
+	
+	public  VentanaVentas () {
+		setSize(700,700);//ESTABLECE EL TAMAÑO DE LA VENTANA
+		setDefaultCloseOperation(EXIT_ON_CLOSE);//DEJA DE EJECUTARSE  AL APRETAR LA CRUZ
+		setTitle("Programa"); //AGREGA EL TIRULO
+		setLocationRelativeTo(null);//COLOCA LA VENTANA EN EL CENTRO
+		cp = getContentPane();
+		iniciarComponentes();
+	}
+	
+	private void iniciarComponentes() {
+		paneles();
+		botones();
+		etiquetas();
+		cuadroDeTexto();
+	}
+	
+	private void cuadroDeTexto() {
+		//getText //para extraer el texto
+	}
+	private void paneles() {
+		p1= new JPanel ();
+		p1.setLayout(null);
+		cp.add(p1);
+	}
+	
+	private void etiquetas() {
+		//VARIABLE DE TEXTO.setHorizontalAlignment(SwingConstants.CENTER);// ALINEA UN TEXTO
+		//b1.setFont(new Font("arial",Font.ITALIC,20));//ESTABLECEMOS LA FUENTE DE TEXTO
+		//ImagenIcon imagen= new ImagenIcon("nombre de la imagen");
+		//DESPUES DE ESTABLECER EL TAMAÑO DE LA ETIQUETA etiqueta.setIcon(new ImagenIcon(imagen.getImagen().getScalendInstance(ancho,largo,imagen.escalado))//AGREGA IMAGEN
+	}
+	
+	private void botones() {
+		//setEnable(); //PARA HABILITAR O DESABILITAR EL BOTON
+		//setForeground(Color.);// cambiar el color de la letra
+		//setFont // cambiar la fuente del texto
+		//setOpaque(true);//para habilitar el cambio delboton
+		//setBackgrond(Color.);// color del boton
+		
+		JButton b1= new JButton("FACTURA");
+		b1.setBounds(10, 10,100,20);
+		p1.add(b1);
+		
+		
+	}
 }
